0597c30c827410561dc167f41f41c018c561d86b
SardiusJay <abidoyejay@gmail.com>
2022-09-15 10:21:08 +0100

    updated

---
+++ index.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $('#one').click(function(){
+        document.forms.display.value += 1;
+    });
+    $(document).ready(function(){
+        $('#two').click(function(){
+            document.forms.display.value += 2;
+        });
+        $(document).ready(function(){
+            $('#three').click(function(){
+                document.forms.display.value += 3;
+            });
+            $(document).ready(function(){
+                $('#four').click(function(){
+                    document.forms.display.value += 4;
+                });
+                $(document).ready(function(){
+                    $('#five').click(function(){
+                        document.forms.display.value += 5;
+                    });
+                    $(document).ready(function(){
+                        $('#six').click(function(){
+                            document.forms.display.value += 6;
+                        });
+                        $(document).ready(function(){
+                            $('#seven').click(function(){
+                                document.forms.display.value += 7;
+                            });
+                            $(document).ready(function(){
+                                $('#eight').click(function(){
+                                    document.forms.display.value += 8;
+                                });
+                                $(document).ready(function(){
+                                    $('#nine').click(function(){
+                                        document.forms.display.value += 9;
+                                    });
+                                    $(document).ready(function(){
+                                        $('#zero').click(function(){
+                                            document.forms.display.value += 0;
+                                        });
+                                        $(document).ready(function(){
+                                            $('#add').click(function(){
+                                                document.forms.display.value += '+';
+                                            });
+                                            $(document).ready(function(){
+                                                $('#clear').click(function(){
+                                                    document.forms.display.value += C;
+                                                });
+                                                $(document).ready(function(){
+                                                    $('#four').click(function(){
+                                                        document.forms.display.value += 4;
+                                                    });
+})
